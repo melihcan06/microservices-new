bf740199b8542a3900902d97f35212dad695d192
Melih Can <melihcankilic@gmail.com>
2023-03-04 15:12:26 +0300

    euraka server eklendi

--- order-service/src/main/java/com/mck/orderservice/constants/Constants.java
+++ order-service/src/main/java/com/mck/orderservice/constants/Constants.java
@@ -1,5 +1,6 @@
 package com.mck.orderservice.constants;
 
 public class Constants {
-    public static final String INVENTORY_URI = "http://localhost:8082/api/inventory";
+    //public static final String INVENTORY_URI = "http://localhost:8082/api/inventory";
+    public static final String INVENTORY_URI = "http://inventory-service/api/inventory";
 }
