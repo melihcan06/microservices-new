10d95a0e0c90e3f242b6abfbd5d59515b5bac3f7
Melih Can <melihcankilic@gmail.com>
2023-03-05 15:50:32 +0300

    keycloak security eklendi. postmanden tokenla istek yapılıyor. auth kısmında

---
+++ api-gateway/src/main/java/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package config;
+
+import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.web.reactive.*;
+import org.springframework.security.config.web.server.*;
+import org.springframework.security.web.server.*;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity){
+        serverHttpSecurity
+                .csrf()
+                .disable()
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/eureka/**")
+                        .permitAll()
+                        .anyExchange()
+                        .authenticated())
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+        return serverHttpSecurity.build();
+    }
+}
